25969460677b4f5f6135f4e110af8d23acc7fe40
VictorRojasLabarca <vrojaslabarca@gmail.com>
2025-01-11 01:26:29 -0300

    Funciones y efectos
    Se agrega funcionalidad a los botones de izquierda y derecha.; se agrega más información del pokémon al buscar Se agregan efectos a los botones al hacer click.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentPokemonId = 1;//Muestra el primer pokémon al cargar la página.
+
 const getPokemonData = async (query) =>{
     try{
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
@@ -9,26 +11,73 @@ const getPokemonData = async (query) =>{
     }
 };
 
-document.querySelector('#searchPokemon').addEventListener('click', async () =>{
-    const query = document.querySelector('#inputPokemon').value.toLowerCase();
-    const pokemonData = await getPokemonData(query);
+// document.querySelector('#searchPokemon').addEventListener('click', async () =>{
+//     const query = document.querySelector('#inputPokemon').value.toLowerCase();
+//     const pokemonData = await getPokemonData(query);
 
-    if(pokemonData){
-        renderPokemonInfo(pokemonData);
-    }else{
-        alert('Pokémon no encontrado, intenta con otro número o nombre');
-    }
-});
+//     if(pokemonData){
+//         renderPokemonInfo(pokemonData);
+//     }else{
+//         alert('Pokémon no encontrado, intenta con otro número o nombre');
+//     }
+// });
 
 const renderPokemonInfo = (pokemon) =>{
+    //información pantalla izquierda - info left display
     const displayLeft = document.querySelector('.display');
     displayLeft.innerHTML = `
         <h2 class='pokemon-name'>${pokemon.name.toUpperCase()}</h2>
         <img class='pokemon-image' src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
     `;
+
+    //Información input - Info input
+    const inputPokemon = document.querySelector('#inputPokemon');
+    inputPokemon.value = pokemon.id;
+
+    //información pantalla derecha - info right display
     const displayRight = document.querySelector('.display-2');
     displayRight.innerHTML = `
         <h4>INFORMACIÓN:</h4>
+        <p>Número ID: ${pokemon.id}</p>
         <p>Tipo: ${pokemon.types.map(type => type.type.name).join(', ')}.</p>
+        <p>Peso: ${pokemon.weight/10} kg.</p>
+        <p>Altura: ${pokemon.height/10} mts.</p>
+        <p>Puntos de salud: ${pokemon.stats[0].base_stat} pts.</p>
+        <p>Puntos de ataque: ${pokemon.stats[1].base_stat} pts.</p>
+        <p>Puntos de defensa: ${pokemon.stats[2].base_stat} pts.</p>
+        <p>Velocidad: ${pokemon.stats[5].base_stat}.</p>
     `;
 };
+
+const fetchAndRenderPokemonInfo = async(id) =>{
+    const pokemonData = await getPokemonData(id);
+    if(pokemonData){
+        currentPokemonId = pokemonData.id;
+        renderPokemonInfo(pokemonData);
+    }else{
+        alert('Pokémon no encontrado, intenta con otro número o nombre');
+    }
+};
+
+//Botón Buscar - Search Button
+document.querySelector('#searchPokemon').addEventListener('click', async() =>{
+    const query = document.querySelector('#inputPokemon').value.toLowerCase();
+    await fetchAndRenderPokemonInfo(query);
+});
+
+//Botón Anterior - Previous Button
+document.querySelector('#prev').addEventListener('click', async() =>{
+    if(currentPokemonId > 1){
+        await fetchAndRenderPokemonInfo(currentPokemonId - 1);
+    }else{
+        alert('Pokémon no encontrado, intenta con otro número o nombre');
+    }
+});
+
+//Botón Siguiente - Next Button
+document.querySelector('#next').addEventListener('click', async() =>{
+        await fetchAndRenderPokemonInfo(currentPokemonId + 1);
+});
+
+//Cargar el primer pokémon al cargar la página - Load the first pokémon when the page loads
+fetchAndRenderPokemonInfo(currentPokemonId);
